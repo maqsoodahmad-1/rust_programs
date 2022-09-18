0ff960720966c858b1cf8a9420e511b18ead744b
maqsoodahmad-1 <maqsodmaxood75@gmail.com>
2022-09-18 22:55:32 +0530

    I was missing the main function

--- chapter11/Box.rs
+++ chapter11/Box.rs
@@ -1,5 +1,9 @@
-let a = 7,  mut a_box:Box<i32>, a_ref:&i32 = &a;
+fn main(){
+let a = 7; 
+let  a_box:Box<i32>;
+let mut a_ref:&i32 = &a;
 println!("{} {};",a, *a_ref);
 a_box = Box::new(a + 2 );
 a_ref = &*a_box;
 println!("{} {} {}",a, *a_ref, *a_box);
+}
